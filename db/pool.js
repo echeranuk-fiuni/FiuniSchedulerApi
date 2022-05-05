f1f833854621bd230baea5325c1a07d29e224951
Your Name <you@example.com>
2022-05-05 13:28:55 -0400

    Heroku dependencies

--- db/pool.js
+++ db/pool.js
@@ -5,5 +5,6 @@ const pool = new Pool({
   database: process.env.DB_NAME,
   password: process.env.DB_PASSWORD,
   port: process.env.DB_PORT,
+  ssl: process.env.DB_SSL === "true" ? { rejectUnauthorized: false} : false,
 })
 module.exports = pool;
